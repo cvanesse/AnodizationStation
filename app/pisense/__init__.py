f4238b5b090e495b9e0d888c1188b53fafcc301b
collin <cvanesse@ualberta.ca>
2018-12-17 08:38:08 -0700

    pisense __init__ cleanup

--- app/pisense/__init__.py
+++ app/pisense/__init__.py
@@ -1,11 +1,3 @@
-#The pisense library holds the sensor base class and classes which handle necessary sensors
-
-#TODO: Implement sensor base class
-#TODO: Implement ina219 sensor
-#   Uses the pi-ina219 library
-#TODO: Implement digital pin sensor
-#   Uses the RPi.GPIO library
-
 #Currently, I don't know of any initialization we need.
 from .currentsensor import CurrentSensor
 from .digipinsensor import DigiPinSensor
