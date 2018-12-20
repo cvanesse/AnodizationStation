7c3decaa25d58e1fa5b74fc749127942178e3ca8
collin <cvanesse@ualberta.ca>
2018-12-19 20:06:40 -0700

    Built elementary Cell class

--- app/cell.py
+++ app/cell.py
@@ -6,4 +6,22 @@
 from .cycle import Cycle
 from .gpiocontrol import Bus
 from .logger import CSVLog
+from .pisense import CurrentSensor
 
+
+class Cell:
+
+    cell_cycle = Cycle()
+
+    def __init__(self, buspins, logfile, sensoraddress):
+        self.cell_bus = Bus([buspins])
+        self.cell_log = CSVLog(logfile, ['Time', 'Current'])
+        self.cell_current_sensor = CurrentSensor(sensoraddress)
+
+    def set_cycle(self, newcycle):
+        self.cell_cycle = newcycle
+
+    def run_cycle(self, numcycles):
+        for i in range(numcycles):
+            for j in range(self.cell_cycle.allcyclecommands):
+                self.cell_cycle.pop()
