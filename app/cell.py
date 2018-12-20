bf3b2f44fef843a61c066c7df27b8a33c3fbbf49
collin <cvanesse@ualberta.ca>
2018-12-20 07:55:34 -0700

    Cell Refactor

--- app/cell.py
+++ app/cell.py
@@ -12,31 +12,32 @@ import time
 
 class Cell:
 
-    cell_cycle = Cycle()
+    cycle = Cycle()
+    bus = []
 
     def __init__(self, buspins, logfile, sensoraddress):
-        self.cell_bus = Bus([buspins])
-        self.cell_log = CSVLog(logfile, ['Time [s]', 'Current [mA]'])
-        self.cell_current_sensor = CurrentSensor(sensoraddress)
+        self.bus = Bus([buspins])
+        self.log = CSVLog(logfile, ['Time [s]', 'Current [mA]'])
+        self.current_sensor = CurrentSensor(sensoraddress)
 
     def set_cycle(self, newcycle):
-        self.cell_cycle = newcycle
+        self.cycle = newcycle
 
     def run_cycle(self, numcycles):
         for i in range(numcycles):
-            for j in range(len(self.cell_cycle.allcyclecommands)):
-                self.cell_cycle.pop()
+            for j in range(len(self.cycle.allcyclecommands)):
+                self.cycle.pop()
 
     # Cell.log handles logging the current time and all the sensors of the cell
     def log(self):
-        self.cell_log.write([time.clock(), self.cell_current_sensor.read()])
+        self.log.write([time.clock(), self.current_sensor.read()])
 
     # Cell.time_delay does a time delay for the given amount of seconds while still logging
     def time_delay(self, seconds):
         start = time.clock()
         current = start
         while current - start < seconds:
-            self.cell_log.write([current, self.cell_current_sensor.read()])
+            self.log.write([current, self.current_sensor.read()])
             current = time.clock()
 
     # Cell.charge_delay does a charge delay for the given amount of seconds while still logging
@@ -44,14 +45,14 @@ class Cell:
         curcharge = 0
         then = time.clock()
         while curcharge < charge:
-            current = self.cell_current_sensor.read()
+            current = self.current_sensor.read()
             now = time.clock()
             curcharge += current*(now - then)
-            self.cell_log.write([now, current])
+            self.log.write([now, current])
             then = now
 
     def set_bus_state(self, sid):
         if sid == 1:
-            self.cell_bus.setstate([1, 1, 1, 1])
+            self.bus.setstate([1, 1, 1, 1])
         else:
-            self.cell_bus.setstate([0, 0, 0, 0])
+            self.bus.setstate([0, 0, 0, 0])
