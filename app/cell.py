adebd57daf53042034789633ddd06980f1dce742
collin <cvanesse@ualberta.ca>
2018-12-19 19:40:53 -0700

    Created cell.py

---
+++ app/cell.py
@@ -0,0 +1,9 @@
+# The base cell class holds:
+#   A Bus
+#   A CSVLog for logging
+#   A Cycle
+#       All Cycle management functions
+from .cycle import Cycle
+from .gpiocontrol import Bus
+from .logger import CSVLog
+
