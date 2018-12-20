b0de4b3c8d5205925d3b55a7dc9a7d619281053b
collin <cvanesse@ualberta.ca>
2018-12-19 20:44:57 -0700

    Added some more functionality to the Cell class

--- app/cell.py
+++ app/cell.py
@@ -7,6 +7,7 @@ from .cycle import Cycle
 from .gpiocontrol import Bus
 from .logger import CSVLog
 from .pisense import CurrentSensor
+import time
 
 
 class Cell:
@@ -15,7 +16,7 @@ class Cell:
 
     def __init__(self, buspins, logfile, sensoraddress):
         self.cell_bus = Bus([buspins])
-        self.cell_log = CSVLog(logfile, ['Time', 'Current'])
+        self.cell_log = CSVLog(logfile, ['Time [s]', 'Current [mA]'])
         self.cell_current_sensor = CurrentSensor(sensoraddress)
 
     def set_cycle(self, newcycle):
@@ -25,3 +26,32 @@ class Cell:
         for i in range(numcycles):
             for j in range(len(self.cell_cycle.allcyclecommands)):
                 self.cell_cycle.pop()
+
+    # Cell.log handles logging the current time and all the sensors of the cell
+    def log(self):
+        self.cell_log.write([time.clock(), self.cell_current_sensor.read()])
+
+    # Cell.time_delay does a time delay for the given amount of seconds while still logging
+    def time_delay(self, seconds):
+        start = time.clock()
+        current = start
+        while current - start < seconds:
+            self.cell_log.write([current, self.cell_current_sensor.read()])
+            current = time.clock()
+
+    # Cell.charge_delay does a charge delay for the given amount of seconds while still logging
+    def charge_delay(self, charge):
+        curcharge = 0
+        then = time.clock()
+        while curcharge < charge:
+            current = self.cell_current_sensor.read()
+            now = time.clock()
+            curcharge += current*(now - then)
+            self.cell_log.write([now, current])
+            then = now
+
+    def set_bus_state(self, sid):
+        if sid == 1:
+            self.cell_bus.setstate([1, 1, 1, 1])
+        else:
+            self.cell_bus.setstate([0, 0, 0, 0])
