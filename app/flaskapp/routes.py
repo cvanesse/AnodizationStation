56bc9a6dc6c904af4d0f3b935802a975b0968559
collin <cvanesse@ualberta.ca>
2018-12-26 12:21:13 -0700

    Added title bar, nav bar, and planned pages to the flask filesystem.

--- app/flaskapp/routes.py
+++ app/flaskapp/routes.py
@@ -1,6 +1,19 @@
 from ..flaskapp import FLASK_APP
+from flask import render_template
 
 @FLASK_APP.route('/')
 @FLASK_APP.route('/index')
+@FLASK_APP.route('/cellcontrol')
 def index():
-    return "Hello World!"
+    title = 'Cell Control'
+    return render_template("cellcontrol.html", title=title)
+
+@FLASK_APP.route('/cycles')
+def cycles():
+    title = 'Cycles'
+    return render_template("cycles.html", title=title)
+
+@FLASK_APP.route('/logs')
+def logs():
+    title = "Logs"
+    return render_template("logs.html", title=title)
