537842cc878dc5c66943ed6071ac97a682c127e3
collin <cvanesse@ualberta.ca>
2018-12-26 20:11:46 -0700

    Added secret_key to protect against CSRF attacks

--- app/flaskapp/__init__.py
+++ app/flaskapp/__init__.py
@@ -1,11 +1,10 @@
 # This package contains all files needed to run the flask webserver which runs on the station
 from flask import Flask
 import os
-
-FLASK_APP = Flask(__name__)
-
 from ..flaskapp import routes
 
+FLASK_APP = Flask(__name__)
+FLASK_APP.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'nchvisfnitsyci8435r34uiwt89'
 
 def start_webserver():
     os.environ["FLASK_APP"] = "../main.py"
