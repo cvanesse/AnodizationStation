fbcfc2c7dab2383c25dbb8f900a0595982d31041
collin-pi <cvanesse@ualberta.ca>
2018-12-30 18:22:53 +0000

    Made the app work with gunicorn, changed the base user.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # This package contains all files needed to run the flask webserver which runs on the station
 from flask import Flask
 from flask_login import LoginManager
+from werkzeug.contrib.fixers import ProxyFix
 import os
 
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
@@ -12,3 +13,8 @@ FLASK_LOGIN = LoginManager(FLASK_APP)
 FLASK_LOGIN.login_view = 'render_login_page'
 
 from app import routes
+
+FLASK_APP.wsgi_app = ProxyFix(FLASK_APP.wsgi_app)
+
+if __name__ == '__main__':
+    FLASK_APP.run()
